053efc3f58ad245833052d8c4705cb7daf0e08d8
Sidswipe <sidswipexyz@gmail.com>
2024-02-15 20:48:11 +0100

    Update Program.cs
    Added logic for Extra credit scores

--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
-
-int currentAssignments = 5;
+//initialize Variables
+int examAssignments = 5;
 int[] sophia =  { 90, 86, 87, 98, 100, 94, 90 };
 int[] nicolas = { 92, 89, 81, 96, 90, 89 };
 int[] zahirah = { 90, 85, 87, 98, 68, 89, 89, 89 };
 int[] jeong = { 90, 98, 92, 100, 97};
+int[] logan =  { 90, 95, 87, 88, 96, 96 };
+int[] becky =  { 92, 91, 90, 91, 92, 92, 92 };
+int[] chris =  { 84, 86, 88, 90, 92, 94, 96, 98 };
+int[] eric =  { 80, 90, 100, 80, 90, 100, 80, 90 };
+int[] gregor =  { 91, 91, 91, 91, 91, 91, 91 };    
 
-string[] studentNames = {"sophia", "nicolas", "zahirah", "jeong"};
+// Student names
+string[] studentNames = {"sophia", "nicolas", "zahirah", "jeong", "logan", "becky", "chris", "eric", "gregor"};
 
 int[] studentScores = new int[10];
 string currentStudentLetterGrade = "";
@@ -30,12 +36,45 @@ studentScores = sophia;
    {
     studentScores = jeong;
    }
-    int sumAssScores = 0;
-  foreach (int score in studentScores) 
+   else if (currentStudent == "logan")
+    {
+        studentScores = logan;
+        }
+   else if (currentStudent == "becky")
+    {
+        studentScores = becky;
+        }
+else if (currentStudent == "chris")
+    {
+        studentScores = chris;
+        }
+else if (currentStudent == "eric")
+    {
+        studentScores = eric;
+        }
+else if (currentStudent == "gregor")
+    {
+    studentScores = gregor;
+    }
+else
+    continue;
+
+    int sumAssScores = 0; // initialize/reset the sum of scored assignments
+    int gradedAssignments = 0; // initialize/reset a counter for the number of assignment
+  foreach (int score in studentScores) // loop through the studentScores array and complete calculations for currentStudent
 {
-    sumAssScores += score;
+    gradedAssignments ++; // increment the assignment counter
+    if (gradedAssignments <= examAssignments)
+    {
+    sumAssScores += score;   // add the exam score to the sum
+    }
+
+    else 
+    {
+        sumAssScores += score / 10; // add the extra credit points to the sum - bonus points equal to 10% of an exam score
+    }
 }
-  decimal currentStudentGrade = (decimal)sumAssScores / currentAssignments;
+  decimal currentStudentGrade = (decimal)sumAssScores / examAssignments;
    
 
 if (currentStudentGrade >= 97)
